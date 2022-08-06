cf209b63faebfd34cb56d61fd4e200abb262803c
Saffire <thesafaladk@gmail.com>
2022-08-06 14:23:34 +0545

    Refactored Code for function calls

--- tweetSource/tweetSource.py
+++ tweetSource/tweetSource.py
@@ -1,14 +1,13 @@
-#Fetches tweets into list fetchedTweets
-#Fetches top 5 most popular contexts/topics of the username (if available) into set topFive 
-#Import and call run(username)
+# Fetches tweets into list fetchedTweets
+# Fetches top 5 most popular contexts/topics of the username (if available) into set topFive
+# Import and call run(username)
 
 import requests
 import yaml
 import json
 
- 
 
-#the main function
+# the main function
 def run(username, FETCHCOUNT=20):
     bearerToken = parse_yaml()
 
@@ -25,15 +24,13 @@ def run(username, FETCHCOUNT=20):
     #     if len(fetchedTweets) > 0:
     #         print("Only Tweets Loaded, There were no contexts.")
 
-    fetchedTweets, fetchedContexts= getTweetsandContextList(jsonObj)
+    fetchedTweets, fetchedContexts = getTweetsandContextList(jsonObj)
     topFiveContexts = getTopFiveContexts(fetchedContexts)
-
-
 
     return fetchedTweets, topFiveContexts
 
 
-#parses the yaml and obtains ApiToken
+# parses the yaml and obtains ApiToken
 def parse_yaml():
     with open("config.yaml", "r") as fileObj:
         try:
@@ -44,46 +41,36 @@ def parse_yaml():
             print(excep)
 
 
-#Finds the top five most talked about topics
+# Finds the top five most talked about topics
 def getTopFiveContexts(fetchedContexts, topX=5):
-    
+
     contexts = {}
     for item in set(fetchedContexts):
         contexts[item] = fetchedContexts.count(item)
 
-    contexts= list(sorted(contexts.items() , key= lambda x: x[1], reverse = True) )
-    #print(contexts)
-
+    contexts = list(sorted(contexts.items(), key=lambda x: x[1], reverse=True))
+    # print(contexts)
 
     return contexts[:topX]
-        
-
-
-
-    # if len(topFive) != 5:
-    #     highest = max(set(fetchedContexts), key=fetchedContexts.count)
-    #     # print(highest)
-    #     topFive.add(highest)
-    #     getTopFiveContexts(list(filter(lambda a: a != highest, fetchedContexts)))
-    # else:
-    #     pass
 
 
 def getTweetsandContextList(jsonObj):
-    fetchedTweets=[]
+    fetchedTweets = []
+    fetchedContexts = []
     for tweetDict in jsonObj["data"]:
         # tweetDict for individual tweet
         fetchedTweets.append(tweetDict["text"])
 
-        #TODO: try 
-
+    try:
         for contexts in tweetDict["context_annotations"]:
             fetchedContexts.append(contexts["entity"]["name"])
+    except:
+        pass
 
     return fetchedTweets, fetchedContexts
 
 
-#requests api for userID
+# requests api for userID
 def getUserID(bearerToken, username):
 
     urlForId = f"https://api.twitter.com/2/users/by/username/{username}"
@@ -91,12 +78,13 @@ def getUserID(bearerToken, username):
     response = requests.request("GET", urlForId, headers=headers)
 
     if response.status_code != 200:
-        raise Exception("Error {}: {}".format(response.status_code, response.text))
+        raise Exception("Error {}: {}".format(
+            response.status_code, response.text))
 
     return response.json()["data"]["id"]
 
 
-#requests api for tweets
+# requests api for tweets
 def getTweets(bearerToken, userID, FETCHCOUNT):
     urlForTweets = f"https://api.twitter.com/2/users/{userID}/tweets?max_results={FETCHCOUNT}&exclude=replies,retweets&tweet.fields=context_annotations"
     headers = {"Authorization": "Bearer {}".format(bearerToken)}
@@ -105,4 +93,6 @@ def getTweets(bearerToken, userID, FETCHCOUNT):
 
 
 if __name__ == "__main__":
-    run("elonmusk")
+    x, y = run("elonmusk")
+    print(x)
+    print(y)
